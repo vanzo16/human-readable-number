d0c8f379127a2e0c075b043baf6e1ff8b79dd42a
vanzo16 <vanzo16@ya.ru>
2021-03-14 14:04:19 +0300

    add task human-readable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function toReadable (number) {
-  
+var a = ['-','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+var b = ['-', '-', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+
+function toReadable (num) {
+    if ((num = num.toString()).length > 9) return 'overflow';
+    n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+    if (!n) return; var str = '';
+    str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
+    str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
+    str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + '- result' : '';
+    return str;
 }
+
+console.log(toReadable(150))
+
